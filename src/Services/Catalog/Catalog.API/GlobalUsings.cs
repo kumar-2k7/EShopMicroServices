a607be03f00d7965a6f22503920ee7b445516c26
Ajay Kumar <41869197+kumar-2k7@users.noreply.github.com>
2026-05-27 11:31:06 +0530

    Section 6 and Section 7 Validation Part (#3)
    1. Section 6- Developed Catalog.API  Infrastructure, Handler and Enpoint Classes for CRUD
    2. Section 7- Developed Catalog.API Validators CCC

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -1,5 +1,8 @@
 ﻿global using BuildingBlocks.CQRS;
 global using Carter;
+global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
+global using FluentValidation;
 global using Mapster;
+global using Marten;
 global using MediatR;
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -3,13 +3,35 @@
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
+    internal class CreateProductHandler(IDocumentSession session, ILogger<CreateProductHandler> logger)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // create Product entity from command object
             // save to database
             // return CreateProductResult result
+
+            logger.LogInformation("Handling CreateProductCommand for command: {command}", command);
+
+            //var result= await validator.ValidateAsync(command, cancellationToken);
+            //var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+            //if (errors.Any())
+            //{
+            //    throw new ValidationException(errors.FirstOrDefault());
+            //}
 
             var product = new Product
             {
@@ -20,7 +42,10 @@
                 Price = command.Price
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
