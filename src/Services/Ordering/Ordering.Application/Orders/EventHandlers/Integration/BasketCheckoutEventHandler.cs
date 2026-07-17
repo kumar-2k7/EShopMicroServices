7a4f1811c49f5afca1cfd51277c893b89599b110
Ajay Kumar <kumar-ajay@hotmail.com>
2026-07-17 10:50:55 +0530

    Enable async messaging with MassTransit & RabbitMQ
    - 🟢 Added MassTransit RabbitMQ integration for event-driven comms
    - 🟢 Introduced DTOs, events, and handlers for basket checkout
    - 🟢 Implemented event consumption and order creation in Ordering
    - 🟢 Updated Docker/compose for RabbitMQ and Ordering service
    - 🟢 Upgraded Mapster, MediatR; enabled feature management
    - 🟢 Minor code and config cleanups
    
    - Ajay Kumar

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration
+{
+    public class BasketCheckoutEventHandler(ISender sender, ILogger<BasketCheckoutEventHandler> logger)
+        : IConsumer<BasketCheckoutEvent>
+    {
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            // TODO: Create new order and start order fullfillment process
+            logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+            var command = MapToCreateOrderCommand(context.Message);
+            await sender.Send(command);
+        }
+
+        private CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+        {
+            // Create full order with incoming event data
+            var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+            var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.CVV, message.PaymentMethod);
+            var orderId = Guid.NewGuid();
+
+            var orderDto = new OrderDto(
+                Id: orderId,
+                CustomerId: message.CustomerId,
+                OrderName: message.UserName,
+                ShippingAddress: addressDto,
+                BillingAddress: addressDto,
+                Payment: paymentDto,
+                Status: Ordering.Domain.Enums.OrderStatus.Pending,
+                OrderItems:
+                [
+                    new OrderItemDto(orderId, new Guid("BBBBBBBB-BBBB-BBBB-BBBB-BBBBBBBBBBBB"), 2, 500),
+                    new OrderItemDto(orderId, new Guid("DDDDDDDD-DDDD-DDDD-DDDD-DDDDDDDDDDDD"), 1, 400)
+                ]);
+
+            return new CreateOrderCommand(orderDto);
+        }
+    }
+}
